be1ff3d94fdc2c9a06e5542fc2d7944b9cacbd35
Jesse Hoobergs <jesse@codebergs.com>
2022-12-15 21:46:11 +0100

    Initial code

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,146 @@
-extern crate peg;
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
 use linked_hash_map::LinkedHashMap;
+use pest::iterators::{Pair, Pairs};
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "yaml.pest"]
+struct YamlParser;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum HashData {
+    InlineComment(String),
+    Comment(String),
+    Element(HashElement),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct HashElement {
+    key: String,
+    value: AliasedYaml,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ArrayData {
+    InlineComment(String),
+    Comment(String),
+    Element(AliasedYaml),
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Yaml {
-    Integer(isize),
+    Hash(Vec<HashData>),
+    Array(Vec<ArrayData>),
     String(String),
-    Hash(LinkedHashMap<String, Yaml>),
-    Array(Vec<Yaml>),
-}:
-
-peg::parser! {
-  grammar parser() for str {
-    rule number() -> Yaml
-      = n:$(['0'..='9']+) {? n.parse().map(|v| Yaml::Integer).or(Err("number")) }
-
-    rule whitespace() = quiet!{[' ' | '\n' | '\t']+}
-
-    pub rule inline_list() -> Vec<u32>
-      = "[" l:(number() ** ",") "]" { l }
-
-    rule string() -> String
-        = 
-
-    pub rule hash() -> Yaml
-      = 
-
-    pub rule document() -> Yaml
-      = hash() / array()
-
-    pub rule yaml() -> Yaml
-      = "---" whitespace() x:document() { x }
-  }
+    Anchor(String),
 }
 
-pub fn main() {
-    assert_eq!(
-        list_parser::list("[1,1,2,3,5,8]"),
-        Ok(vec![1, 1, 2, 3, 5, 8])
-    );
+#[derive(Debug, Clone, PartialEq)]
+pub struct AliasedYaml {
+    alias: Option<String>,
+    value: Yaml,
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum DocumentData {
+    Comment(String),
+    Yaml(Yaml),
+}
+
+use pest::error::Error;
+
+fn parse_yaml_file(file: &str) -> Result<Vec<DocumentData>, Error<Rule>> {
+    let pairs = YamlParser::parse(Rule::yaml, file)?;
+    Ok(pairs
+        .map(|p| match p.as_rule() {
+            Rule::document => DocumentData::Yaml(parse_value(p.into_inner().next().unwrap())),
+            Rule::commentnl | Rule::comment => DocumentData::Comment(comment(p)),
+            _ => unreachable!(),
+        })
+        .collect())
+}
+
+fn comment(pair: Pair<Rule>) -> String {
+    pair.into_inner().next().unwrap().as_str().to_string()
+}
+
+fn parse_value(pair: Pair<Rule>) -> Yaml {
+    match pair.as_rule() {
+        Rule::hash => Yaml::Hash(
+            pair.into_inner()
+                .map(|pair| parse_hash_data(pair))
+                .collect(),
+        ),
+        Rule::array => Yaml::Array(
+            pair.into_inner()
+                .map(|pair| parse_array_data(pair))
+                .collect(),
+        ),
+        Rule::string => Yaml::String(pair.into_inner().next().unwrap().as_str().to_string()),
+        Rule::anchor => Yaml::Anchor(pair.into_inner().next().unwrap().as_str().to_string()),
+        _ => unreachable!("{:?}", pair),
+    }
+}
+
+fn parse_hash_data(pair: Pair<Rule>) -> HashData {
+    match pair.as_rule() {
+        Rule::hash_element => HashData::Element(parse_hash_element(pair.into_inner())),
+        Rule::commentnl => HashData::Comment(comment(pair)),
+        Rule::comment => HashData::InlineComment(comment(pair)),
+        _ => unreachable!(),
+    }
+}
+
+fn parse_hash_element(mut pairs: Pairs<Rule>) -> HashElement {
+    let key = pairs.next().unwrap().as_str().to_string();
+    let item2 = pairs.next().unwrap();
+    return HashElement {
+        key,
+        value: if let Some(value) = pairs.next() {
+            AliasedYaml {
+                alias: Some(item2.as_str().to_string()),
+                value: parse_value(value),
+            }
+        } else {
+            AliasedYaml {
+                alias: None,
+                value: parse_value(item2),
+            }
+        },
+    };
+}
+
+fn parse_array_data(pair: Pair<Rule>) -> ArrayData {
+    match pair.as_rule() {
+        Rule::commentnl => ArrayData::Comment(comment(pair)),
+        Rule::comment => ArrayData::InlineComment(comment(pair)),
+        Rule::block_array_element => ArrayData::Element(parse_array_element(pair.into_inner())),
+        _ => ArrayData::Element(AliasedYaml {
+            alias: None,
+            value: parse_value(pair),
+        }),
+    }
+}
+
+fn parse_array_element(mut pairs: Pairs<Rule>) -> AliasedYaml {
+    let item1 = pairs.next().unwrap();
+
+    if let Some(value) = pairs.next() {
+        AliasedYaml {
+            alias: Some(item1.as_str().to_string()),
+            value: parse_value(value),
+        }
+    } else {
+        AliasedYaml {
+            alias: None,
+            value: parse_value(item1),
+        }
+    }
+}
+/*
 #[cfg(test)]
 mod tests {
     use super::parser;
@@ -48,4 +149,4 @@ mod tests {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
-}
+}*/
